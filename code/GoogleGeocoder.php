3879114848dc32f7f4f53d6dff309d21f1b92c97
Mark Guinn <mark@adaircreative.com>
2015-12-08 14:05:29 +0000

    Updated geocoder class to use tractorcow module

--- code/GoogleGeocoder.php
+++ code/GoogleGeocoder.php
@@ -5,7 +5,7 @@ define("GOOGLEMAPS_HOST", "maps.google.com");
 /**
  * Server-side connector to the Google Geocoding API.
  * Uses GOOGLEMAPS_API_KEY for the API key
- * 
+ *
  * Usage:
  * <pre>
  * $g = new GoogleGeocoder();
@@ -14,45 +14,49 @@ define("GOOGLEMAPS_HOST", "maps.google.com");
  * </pre>
  */
 class GoogleGeocoder {
-	
+
+	/**
+	 * @var IGeocodingService
+	 */
+	protected $service;
+
+
+	/**
+	 * @return IGeocodingService
+	 */
+	public function getService() {
+		if (!isset($this->service)) {
+			$this->service = Injector::inst()->get('GeocodingService');
+		}
+		return $this->service;
+	}
+
+	/**
+	 * @param IGeocodingService $service
+	 * @return $this
+	 */
+	public function setService($service) {
+		$this->service = $service;
+		return $this;
+	}
 
 	/**
 	 * Returns a Google Geopoint matching the given address parts
 	 */
-	function addressPartsToPoint($streetNumber, $street, $suburb, $city, $country) {
+	public function addressPartsToPoint($streetNumber, $street, $suburb, $city, $country) {
 		return $this->addressToPoint("$streetNumber, $street, $suburb, $city, $country");
 	}
 
 	/**
 	 * Returns a Google Geopoint matching the given address.
 	 */
-	function addressToPoint($address) {
-		$baseURL = "http://" . GOOGLEMAPS_HOST . "/maps/geo?output=xml" . "&key=" . GOOGLEMAPS_API_KEY;
-		$requestURL = $baseURL . "&q=" . urlencode($address);
-		
-		$delay = 0;
-		
-		$status = "620";
-		while(strcmp($status, "620") == 0) {		
-	    	$xml = simplexml_load_file($requestURL) or user_error("Can't access $url", E_USER_WARNING);
-			if(!$xml) return;
-			
-		    $status = $xml->Response->Status->code;
-		    if (strcmp($status, "200") == 0) {
-				// Successful geocode
-				$coordinates = $xml->Response->Placemark->Point->coordinates;
-				$coordinatesSplit = split(",", $coordinates);
-				// Format: Longitude, Latitude, Altitude
-				$lat = $coordinatesSplit[1];
-				$lng = $coordinatesSplit[0];
-				return GeoPoint::from_x_y($lng, $lat);
-
-		    } else if (strcmp($status, "620") == 0) {
-				// sent geocodes too fast
-				$delay += 100000;
-			}
+	public function addressToPoint($address) {
+		$result = $this->getService()->geocode($address);
+		if ($result['Success']) {
+			return GeoPoint::from_x_y($result['Longitude'], $result['Latitude']);
+		} else {
+			user_error("Could not geocode address: " . $result['Message'], E_USER_ERROR);
 		}
 	}
 }
 
-?>
